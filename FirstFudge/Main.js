89ee011d9859abae580c944297ca6085f3c509b7
Benjamin Brüggemann <ben-brueggemann@web.de>
2020-04-21 18:07:30 +0200

    FirstFudge

--- FirstFudge/Main.js
+++ FirstFudge/Main.js
@@ -1,6 +1,11 @@
 "use strict";
 var FirstFudge;
 (function (FirstFudge) {
-    console.log("Hallo");
+    var f = FudgeCore;
+    let node = new f.Node("TestNode");
+    console.log(node);
+    //Viewport 
+    //Camera
+    //Node
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- FirstFudge/Main.ts
+++ FirstFudge/Main.ts
@@ -1,3 +1,10 @@
 namespace FirstFudge {
-    console.log("Hallo");
+    import f = FudgeCore;
+
+    let node: f.Node = new f.Node("TestNode");
+    console.log(node);
+
+    //Viewport 
+    //Camera
+    //Node
 }
