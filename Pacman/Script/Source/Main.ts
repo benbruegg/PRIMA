a98d4ac5c52e59cb2e79af941d4aac50f42a20eb
unknown <ben-brueggemann@web.de>
2022-03-24 14:56:48 +0100

    controls update

--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -1,19 +1,42 @@
 namespace Script {
   import ƒ = FudgeCore;
   ƒ.Debug.info("Main Program Template running!");
-
   let viewport: ƒ.Viewport;
+  let pacman: ƒ.Node;
+  let speed: ƒ.Vector3 = new ƒ.Vector3(0,0,0);
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    let graph: ƒ.Node = viewport.getBranch();
+    pacman = graph.getChildrenByName("Pacman")[0];
+    console.log(pacman);
+
+    // pacman.mtxLocal.translate(new ƒ.Vector3(1, 1, 0));
+
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    // tslint:disable-next-line: max-line-length
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && pacman.mtxLocal.translation.y%1 < 0.05) {
+      speed.set(1/60,0,0);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])&& pacman.mtxLocal.translation.y%1 < 0.05) {
+      speed.set(-1/60,0,0);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])&& pacman.mtxLocal.translation.x%1 < 0.05) {
+      speed.set(0,1/60,0);
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])&& pacman.mtxLocal.translation.x%1 < 0.05) {
+      speed.set(0,-1/60,0);
+    }
+
+    pacman.mtxLocal.translate(speed);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
