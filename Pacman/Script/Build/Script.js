ccaa151dd951d56f41c2c67360d1698e5d064a3c
unknown <ben-brueggemann@web.de>
2022-03-29 11:34:17 +0200

    messy collision added

--- Pacman/Script/Build/Script.js
+++ Pacman/Script/Build/Script.js
@@ -48,7 +48,7 @@ var Script;
         viewport = _event.detail;
         let graph = viewport.getBranch();
         pacman = graph.getChildrenByName("Pacman")[0];
-        console.log(pacman);
+        console.log(graph);
         // pacman.mtxLocal.translate(new ƒ.Vector3(1, 1, 0));
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         // tslint:disable-next-line: max-line-length
@@ -56,21 +56,85 @@ var Script;
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && pacman.mtxLocal.translation.y % 1 < 0.05) {
+        // tslint:disable-next-line: max-line-length
+        if (!isWall(Math.round(pacman.mtxLocal.translation.x + 0.51), Math.round(pacman.mtxLocal.translation.y)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
             speed.set(1 / 60, 0, 0);
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && pacman.mtxLocal.translation.y % 1 < 0.05) {
+        // tslint:disable-next-line: max-line-length
+        if (!isWall(Math.round(pacman.mtxLocal.translation.x - 0.51), Math.round(pacman.mtxLocal.translation.y)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
             speed.set(-1 / 60, 0, 0);
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && pacman.mtxLocal.translation.x % 1 < 0.05) {
+        // tslint:disable-next-line: max-line-length
+        if (!isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y + 0.51)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
             speed.set(0, 1 / 60, 0);
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && pacman.mtxLocal.translation.x % 1 < 0.05) {
+        // tslint:disable-next-line: max-line-length
+        if (!isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y - 0.51)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
             speed.set(0, -1 / 60, 0);
+        }
+        if (isWall(Math.round(pacman.mtxLocal.translation.x + 0.51), Math.round(pacman.mtxLocal.translation.y)) && Math.sign(speed.x) === 1) {
+            speed.set(0, 0, 0);
+        }
+        if (isWall(Math.round(pacman.mtxLocal.translation.x - 0.51), Math.round(pacman.mtxLocal.translation.y)) && Math.sign(speed.x) === -1) {
+            speed.set(0, 0, 0);
+        }
+        if (isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y + 0.51)) && Math.sign(speed.y) === 1) {
+            speed.set(0, 0, 0);
+        }
+        if (isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y - 0.51)) && Math.sign(speed.y) === -1) {
+            speed.set(0, 0, 0);
         }
         pacman.mtxLocal.translate(speed);
         viewport.draw();
         ƒ.AudioManager.default.update();
+    }
+    function isTile(_x, _y) {
+        let graph = viewport.getBranch();
+        let grid = graph.getChildrenByName("Grid")[0];
+        // the row of the grid (y-direction)
+        let row = grid.getChildren()[_y];
+        // get the Element in x-direction
+        console.log(row);
+        if (row !== undefined) {
+            let rowElement = row.getChild(_x);
+            console.log(rowElement);
+            if (rowElement !== undefined && row !== undefined) {
+                console.log("Ist ein Feld");
+                return true;
+            }
+        }
+        else {
+            console.log("Ist kein Feld");
+            return false;
+        }
+        return false;
+    }
+    function isWall(_x, _y) {
+        // get the Grid of all rows
+        let graph = viewport.getBranch();
+        let grid = graph.getChildrenByName("Grid")[0];
+        console.log(isTile(_x, _y));
+        if (isTile(_x, _y)) {
+            // the row of the grid (y-direction)
+            let row = grid.getChildren()[_y];
+            // get the Element in x-direction
+            let rowElement = row.getChild(_x);
+            console.log(rowElement);
+            let material = rowElement.getComponent(ƒ.ComponentMaterial);
+            // get Element Color
+            let color = material.clrPrimary;
+            // get individual RGB values
+            let clrR = color.r;
+            let clrG = color.g;
+            let clrB = color.b;
+            console.log(clrR, clrG, clrB);
+            if (clrR === 1 && clrG === 1 && clrB === 1) {
+                console.log("Ist keine Wand");
+                return false;
+            }
+        }
+        console.log("Ist eine Wand");
+        return true;
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -3,15 +3,14 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
-  let speed: ƒ.Vector3 = new ƒ.Vector3(0,0,0);
+  let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
     let graph: ƒ.Node = viewport.getBranch();
     pacman = graph.getChildrenByName("Pacman")[0];
-    console.log(pacman);
-
+    console.log(graph);
     // pacman.mtxLocal.translate(new ƒ.Vector3(1, 1, 0));
 
 
@@ -23,21 +22,89 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && pacman.mtxLocal.translation.y%1 < 0.05) {
-      speed.set(1/60,0,0);
+    // tslint:disable-next-line: max-line-length
+    if (!isWall(Math.round(pacman.mtxLocal.translation.x + 0.51), Math.round(pacman.mtxLocal.translation.y)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
+      speed.set(1 / 60, 0, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])&& pacman.mtxLocal.translation.y%1 < 0.05) {
-      speed.set(-1/60,0,0);
+    // tslint:disable-next-line: max-line-length
+    if (!isWall(Math.round(pacman.mtxLocal.translation.x - 0.51), Math.round(pacman.mtxLocal.translation.y)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
+      speed.set(-1 / 60, 0, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])&& pacman.mtxLocal.translation.x%1 < 0.05) {
-      speed.set(0,1/60,0);
+    // tslint:disable-next-line: max-line-length
+    if (!isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y + 0.51)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+      speed.set(0, 1 / 60, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])&& pacman.mtxLocal.translation.x%1 < 0.05) {
-      speed.set(0,-1/60,0);
+    // tslint:disable-next-line: max-line-length
+    if (!isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y - 0.51)) && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+      speed.set(0, -1 / 60, 0);
     }
+
+    if (isWall(Math.round(pacman.mtxLocal.translation.x + 0.51), Math.round(pacman.mtxLocal.translation.y)) && Math.sign(speed.x) === 1) {
+      speed.set(0, 0, 0);
+    }
+    if (isWall(Math.round(pacman.mtxLocal.translation.x - 0.51), Math.round(pacman.mtxLocal.translation.y)) && Math.sign(speed.x) === -1) {
+      speed.set(0, 0, 0);
+    }
+    if (isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y + 0.51)) && Math.sign(speed.y) === 1) {
+      speed.set(0, 0, 0);
+    }
+    if (isWall(Math.round(pacman.mtxLocal.translation.x), Math.round(pacman.mtxLocal.translation.y - 0.51)) && Math.sign(speed.y) === -1) {
+      speed.set(0, 0, 0);
+    }
+
 
     pacman.mtxLocal.translate(speed);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+  function isTile(_x: number, _y: number): boolean {
+    let graph: ƒ.Node = viewport.getBranch();
+    let grid: ƒ.Node = graph.getChildrenByName("Grid")[0];
+
+    // the row of the grid (y-direction)
+    let row: ƒ.Node = grid.getChildren()[_y];
+    // get the Element in x-direction
+    console.log(row);
+    if (row !== undefined) {
+    let rowElement: ƒ.Node = row.getChild(_x);
+    console.log(rowElement);
+    if (rowElement !== undefined && row !== undefined) {
+      console.log("Ist ein Feld");
+      return true;
+    }
+    } else {
+      console.log("Ist kein Feld");
+      return false;
+    }
+    return false;
+  }
+  function isWall(_x: number, _y: number): boolean {
+    // get the Grid of all rows
+    let graph: ƒ.Node = viewport.getBranch();
+    let grid: ƒ.Node = graph.getChildrenByName("Grid")[0];
+    console.log(isTile(_x, _y));
+    if (isTile(_x, _y)) {
+      // the row of the grid (y-direction)
+      let row: ƒ.Node = grid.getChildren()[_y];
+      // get the Element in x-direction
+      let rowElement: ƒ.Node = row.getChild(_x);
+      console.log(rowElement);
+      let material: ƒ.ComponentMaterial = rowElement.getComponent(ƒ.ComponentMaterial);
+      // get Element Color
+      let color: ƒ.Color = material.clrPrimary;
+      // get individual RGB values
+      let clrR: number = color.r;
+      let clrG: number = color.g;
+      let clrB: number = color.b;
+      console.log(clrR, clrG, clrB);
+      if (clrR === 1 && clrG === 1 && clrB === 1) {
+        console.log("Ist keine Wand");
+        return false;
+      }
+    }
+    console.log("Ist eine Wand");
+    return true;
+  }
 }
+
+
