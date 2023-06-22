35a8a8493c9a96ffb0f60fdfc7981cf2b9465ef0
unknown <ben-brueggemann@web.de>
2023-06-22 21:35:52 +0200

    only runtime files, my bad

--- FUDGE/Core/Source/Shader/ShaderLitSkin.ts
+++
@@ -1,8 +0,0 @@
-namespace FudgeCore {
-  export abstract class ShaderLitSkin extends Shader {
-    public static readonly iSubclass: number = Shader.registerSubclass(ShaderLitSkin);
-    public static define: string[] = [
-      "SKIN"
-    ];
-  }
-}
